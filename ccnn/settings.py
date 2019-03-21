296631e64d4fd13c7e62b1e668d42eea8abfe5fb
Szymon Balasz <sbalasz@gmail.com>
2019-03-21 23:21:37 +0200

    commented out staticfiles_storage

--- ccnn/settings.py
+++ ccnn/settings.py
@@ -131,6 +131,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
