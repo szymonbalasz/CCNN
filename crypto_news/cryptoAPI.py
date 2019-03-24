980e4fbfc940457be37033b1d4276732bf078b5c
Szymon Balasz <sbalasz@gmail.com>
2019-03-24 23:19:14 +0200

    updated api. ready for accounts settings

--- crypto_news/cryptoAPI.py
+++ crypto_news/cryptoAPI.py
@@ -1,12 +1,28 @@
 import requests
 import json
+from decouple import config
 
-def cPrices():
-	priceRequest = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD")
+def cPrice():
+	headers = {
+		"X-CMC_PRO_API_KEY" : config('API_KEY')
+	}
+
+	priceRequest = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", headers=headers)
 	price = json.loads(priceRequest.content)
+
+	if "data" in price:
+		price = price["data"]
+	else:
+		price = {}
+
 	return price
+
+
 
 def cNews():
 	newsRequest = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
 	news = json.loads(newsRequest.content)
 	return news
+
+def cNewsRelevant(tags):
+	return tags
--- crypto_news/views.py
+++ crypto_news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from . import cryptoAPI
 
 def home(request):
-	price = cryptoAPI.cPrices()
+	price = cryptoAPI.cPrice()
 	news = cryptoAPI.cNews()
 
 	return render(request, 'home.html', {'news' : news, 'price' : price})
